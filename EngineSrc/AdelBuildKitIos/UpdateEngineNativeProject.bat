8187c34ab673788443d8fea4f56e213d7603d0c1
hoboaki <hoboaki@10106.net>
2025-08-10 23:41:58 +0900

    refactor: delete EngineSrc

--- EngineSrc/AdelBuildKitIos/UpdateEngineNativeProject.bat
+++
@@ -1,6 +0,0 @@
-:: 文字コード：UTF-8
-@echo off
-cd /d %~dp0
-call ..\DevelopSupport\WindowsCmd\RequireRoslyn.cmd
-csi ./UpdateEngineNativeProject.csx
-:: EOF
--- EngineSrc/DevelopSupport/PowerShell/MsbuildTest.ps1
+++
@@ -1,5 +0,0 @@
-# テスト実行
-. "./Msbuild.ps1"
-
-$msbuild = New-Object Msbuild
-$msbuild.Execute("/version")
